0eb6e21f11079de8bab7f0a053fdb4a97f5e14b5
dermokill <saadboukili66@gmail.com>
2025-03-06 22:48:37 +0100

    1stCommit

---
+++ Tickefy/src/main/java/com/tickefy/tickefy/TickefyApplication.java
@@ -0,0 +1,14 @@
+package com.tickefy.tickefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickefyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickefyApplication.class, args);
+        System.out.println("Tickefy-MAIN");
+    }
+
+}
