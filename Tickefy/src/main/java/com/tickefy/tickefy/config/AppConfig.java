df7d1710481e8f6de7063127a85211b9d7040f47
Yasmine Znatni <104834084+Assiminee@users.noreply.github.com>
2025-05-06 18:57:32 +0100

    Forgot HTTP method import (added)

--- Tickefy/src/main/java/com/tickefy/tickefy/config/AppConfig.java
+++ Tickefy/src/main/java/com/tickefy/tickefy/config/AppConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-
+import org.springframework.http.HttpMethod;
 import java.util.Arrays;
 import java.util.Collections;
 
