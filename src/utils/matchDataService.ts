f7db03bda455b40a8da98ae42f479161ef46990b
dermokill <saadboukili66@gmail.com>
2025-05-13 19:52:25 +0100

    Removed the dumbass date increment ( genius moroccan solution tho)

--- src/utils/matchDataService.ts
+++ src/utils/matchDataService.ts
@@ -132,7 +132,7 @@ const generateMatches = () => {
     for (let i = 0; i < teams.length; i++) {
       for (let j = i + 1; j < teams.length; j++) {
         const matchDate = new Date(startDate);
-        matchDate.setDate(startDate.getDate() + matchId - 1);
+        matchDate.setDate(startDate.getDate());
 
         matches.push({
           fixture: {
