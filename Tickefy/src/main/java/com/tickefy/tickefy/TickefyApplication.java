cb08d978a6695f7749649fc14be3b48c47965bb1
Assimine <znatni.yasmine@gmail.com>
2025-10-07 13:42:23 +0100

    Minor import issue fixed

--- Tickefy/src/main/java/com/tickefy/tickefy/TickefyApplication.java
+++ Tickefy/src/main/java/com/tickefy/tickefy/TickefyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.tickefy.tickefy.entities.Client;
-import com.tickefy.tickefy.enums.Role;
+import com.tickefy.tickefy.entities.enums.Role;
 
 @SpringBootApplication
 public class TickefyApplication implements CommandLineRunner {
