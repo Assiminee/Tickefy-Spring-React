8d25cf159dec733a34965117b7c65eadc83bcc66
Assimine <znatni.yasmine@gmail.com>
2025-10-07 13:48:45 +0100

    Minor import issue fixed

--- Tickefy/src/main/java/com/tickefy/tickefy/TickefyApplication.java
+++ Tickefy/src/main/java/com/tickefy/tickefy/TickefyApplication.java
@@ -1,14 +1,20 @@
 package com.tickefy.tickefy;
 
+import com.tickefy.tickefy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.tickefy.tickefy.entities.Client;
 import com.tickefy.tickefy.entities.enums.Role;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TickefyApplication implements CommandLineRunner {
+    @Autowired
     private UserRepository repo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(TickefyApplication.class, args);
@@ -25,5 +31,7 @@ public class TickefyApplication implements CommandLineRunner {
         androidClient.setL_name("Client");
         androidClient.setRole(Role.ROLE_ADMIN);
         androidClient.setPassword(passwordEncoder.encode("AndroidClient@1"));
+
+        repo.save(androidClient);
     }
 }
